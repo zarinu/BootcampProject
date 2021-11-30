051a07a3b147ae87f426d017a70f153dda7f1228
sayeban300300@gmail.com <sayeban300300@gmail.com>
2021-11-30 05:40:31 +0330

    edit parent_id type to integer

--- database/migrations/2021_11_29_144341_create_categories_table.php
+++ database/migrations/2021_11_29_144341_create_categories_table.php
@@ -15,6 +15,9 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->integer('parent_id');
+            $table->string('name', 100);
+            $table->string('name_en', 100);
             $table->timestamps();
         });
     }
