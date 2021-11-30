dc2ebc522362a6343b4aa7b715333f087df9cb8c
zahra heydari <zheydari5701@gmail.com>
2021-11-30 16:18:14 +0330

    fix migration of ads table Cos cat_id in ads table before cat table

--- app/Models/Ade.php
+++ app/Models/Ade.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Ad extends Model
+class Ade extends Model
 {
     use HasFactory;
     public function user()
