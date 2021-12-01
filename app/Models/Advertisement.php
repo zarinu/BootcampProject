a699b61b8249245bbe7e60052d260a025af1c19a
zahra heydari <zheydari5701@gmail.com>
2021-12-01 21:01:11 +0330

    rename of Ade.php Model to Advertisement.php

--- app/Models/Advertisement.php
+++ app/Models/Advertisement.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-class Ade extends Model
+class Advertisement extends Model
 {
     use HasFactory;
     // public function user()
