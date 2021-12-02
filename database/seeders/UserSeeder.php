6a0562f9607f1cbbd841e51f5d46bc720dad474d
sayeban300300@gmail.com <sayeban300300@gmail.com>
2021-12-03 02:13:14 +0330

    create faker user data for example 5 user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Auth\User;
 
 class UserTableSeeder extends Seeder
 {
@@ -13,6 +14,13 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker=\Faker\Factory::create();
+        foreach(range(1,5) as $item){
+            User::create([
+                'name'=>$faker->name(50),
+                'email'=>$faker->email(100),
+                'password'=>$faker->password()
+            ]);
+        }
     }
 }
