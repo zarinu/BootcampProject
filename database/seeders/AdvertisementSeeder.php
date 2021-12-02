21931134ab597e35e2e8c899234a6189ebbf9b59
sayeban300300@gmail.com <sayeban300300@gmail.com>
2021-12-03 01:56:17 +0330

    edite class name and imported namespace to file

---
+++ database/seeders/AdvertisementSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Ade;
+use App\Models\Ads;
+use App\Models\Advertisement;
+use Illuminate\Database\Seeder;
+
+class AdsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker=\Faker\Factory::create();
+    foreach(range(1,10) as $item){
+        Advertisement::create([
+            'title'=>$faker->text(20),
+            'desc'=>$faker->text(250),
+            'price'=>$faker->randomDigitNot(0),
+            'mobileNo'=>$faker->phoneNumber(),
+            'adress'=>$faker->address()
+
+        ]);
+    }
+    }
+}
