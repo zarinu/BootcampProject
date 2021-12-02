1d8a141a4580424c5d8859848f23a68ba2b50dd0
zahra heydari <zheydari5701@gmail.com>
2021-12-02 11:31:27 +0330

    to create an merge conflict for test

--- resources/views/layouts/maser.blade.php
+++ resources/views/layouts/maser.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="">
+    <meta charset="kdfgh">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{asset('css/app.css')}}" ></link>
