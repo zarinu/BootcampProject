cec731afe9e81785513f29985454679484b09e14
sayeban300300@gmail.com <sayeban300300@gmail.com>
2021-12-03 03:43:09 +0330

    add func to show category

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    //this controller have show, create, update method..
+    public function show($id){
+
+    //     $category=Category::findOrFail($id);
+    //   return view('ads.ShowCategory',compact('id'));
+      }
+}
