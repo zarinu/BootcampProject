1f429b02b70f22313717347648f402844779dd9c
zahra heydari <zheydari5701@gmail.com>
2021-12-04 06:28:22 +0330

    change categoriesSeeder to categorySeeder in databaseSeeder because it's doesn't exsist

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-       $this->call([CategoriesSeeder::class,AdvertisementSeeder::class,UserSeeder::class]);
+       $this->call([CategorySeeder::class,AdvertisementSeeder::class,UserSeeder::class]);
         // \App\Models\User::factory(10)->create();
 
 
