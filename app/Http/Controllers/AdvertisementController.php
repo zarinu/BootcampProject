9f0a6abf90141585162eafef48cc046cf51a9a91
zahra heydari <zheydari5701@gmail.com>
2021-12-06 05:48:23 +0330

    creation of destroy route

--- app/Http/Controllers/AdvertisementController.php
+++ app/Http/Controllers/AdvertisementController.php
@@ -13,23 +13,25 @@ class AdvertisementController extends Controller
     {
         // $userwiht = User::has('sdkfj')->get;
         // dd($userwiht);
-        $ads = Advertisement::where('user_id', Auth::user()->id)->paginate(5);
+        $ads = Advertisement::where('user_id', Auth::user()->id)->paginate(11);
         return view('index', compact('ads'));
     }
     public function show($adID)
     {
         $ade = Advertisement::where('user_id', Auth::user()->id)->where('id', $adID)->get();
         //check if this $ade does not exist
-        if(empty($ade->toArray())) dd("fuck it empty");
+        if (empty($ade->toArray())) dd("fuck it empty");
         return view('show')->with(['ade' => $ade[0]]);
     }
-    public function create() {
+    public function create()
+    {
         $categories = Category::all();
         return view('create')->with(['categories' => $categories]);
     }
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         //validate kardan request ha badan anjam mishe haji
-    
+
         //inja mikham logic konam
         $ade = new Advertisement();
         $ade->title = $request->title;
@@ -46,16 +48,18 @@ class AdvertisementController extends Controller
 
         return; // 422
     }
-    public function edit($adID) {
+    public function edit($adID)
+    {
         //inja bayad permission mojod and ejaze bedam
         $ade = Advertisement::find($adID);
 
         $categories = Category::all();
         return view('edit')->with(['categories' => $categories, 'ade' => $ade]);
     }
-    public function update(Request $request, $adID) {
+    public function update(Request $request, $adID)
+    {
         //validate kardan request ha badan anjam mishe haji
-    
+
         //inja mikham logic konam
         $ade = Advertisement::find($adID);
         $ade->title = $request->title;
@@ -72,10 +76,21 @@ class AdvertisementController extends Controller
 
         return; // 422
     }
-    public function delete($adID) {
+    public function delete($adID)
+    {
         //inja bayad permission mojod and ejaze bedam
         $ade = Advertisement::find($adID);
 
         return view('delete')->with(['ade' => $ade]);
+    }
+    public function destroy(Request $request, $adID)
+    {
+        //validate kardan request ha badan anjam mishe haji
+
+        //inja mikham logic konam
+
+        $ade = Advertisement::find($adID);
+        $ade->delete();
+        return redirect('/');
     }
 }
