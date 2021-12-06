ea9f635dded0fdc0f20e540444b2afd8a26091cf
sayeban300300@gmail.com <sayeban300300@gmail.com>
2021-12-06 13:00:00 +0330

    compeleted category cruds

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,7 +10,40 @@ class CategoryController extends Controller
     //this controller have show, create, update method..
     public function show($id){
 
-    //     $category=Category::findOrFail($id);
-    //   return view('ads.ShowCategory',compact('id'));
+        $category=Category::findOrFail($id);
+      return view('ads.ShowCategory',compact('id'));
       }
+
+    public function create(){
+        return view('ads.create');
+        }
+
+    public function store(Request $request){
+          $request->validate([
+              'name'=>'required'
+          ]);
+          Category::create([
+              'name'=>$request-> name
+          ]);
+        }
+    public function edit($id){
+            $id=Category::findOrFail($id);
+            return view('ads.edit',compact('id'));
+
+        }
+    public function update(Request $request, $id){
+            $id=Category::findOrFail($id);
+            $request->validate([
+            'name'=>'required'
+            ]);
+            $id->update([
+                'name'=>$request-> name
+                ]);
+            return redirect()->route('ads.index');
+        }
+    public function delete(Request $request, $id){
+            $id=Category::findOrFail($id);
+            $id->delete();
+            return redirect()->route('ads.index');
+        }
 }
