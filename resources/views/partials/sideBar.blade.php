428e58ce49598e5a96858cf6c148c0b9d88ab0cf
sayeban300300@gmail.com <sayeban300300@gmail.com>
2021-12-08 19:40:23 +0330

    sidebare ...

--- resources/views/partials/sideBar.blade.php
+++ resources/views/partials/sideBar.blade.php
@@ -1,8 +1,8 @@
-{{-- <div>
+<div>
     <p > Category</p>
 </div>
 <ul class="list-group">
-    @foreach ($categoreis as $category )
+    @foreach ($categories as $category )
      <li class="list-group-item ">{{$category->name}}</li>
      @endforeach
-  </ul> --}}
+  </ul>
