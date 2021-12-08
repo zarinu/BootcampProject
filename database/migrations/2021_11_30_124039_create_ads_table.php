d09e4b9b4b78f18acbb255eb19cd50f7cc271a78
sayeban300300@gmail.com <sayeban300300@gmail.com>
2021-12-08 13:47:06 +0330

    to organize foregin key

--- database/migrations/2021_11_30_124039_create_ads_table.php
+++ database/migrations/2021_11_30_124039_create_ads_table.php
@@ -16,10 +16,13 @@ class CreateAdsTable extends Migration
         Schema::create('ads', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+
             $table->unsignedBigInteger('category_id');
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade')->onUpdate('cascade');
+
             $table->char('title', 27);
             $table->string('desc', 303);
             $table->integer('price');
