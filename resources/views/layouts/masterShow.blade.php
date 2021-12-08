5353b35158dc6a24f93ad1f495f8469637ede8d0
sayeban300300@gmail.com <sayeban300300@gmail.com>
2021-12-08 12:18:14 +0330

    spicial master layouts for show view seeAds

---
+++ resources/views/layouts/masterShow.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}" ></link>
+    <link rel="stylesheet" href="{{asset('js/app.js')}}" ></link>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title></title>
+</head>
+<body>
+
+        @include('partials.header')
+        <div class="container-fluid p-3  border ">
+            @yield('content')
+        </div>
+        {{-- @include('partials.footer') --}}
+
+</body>
+</html>
