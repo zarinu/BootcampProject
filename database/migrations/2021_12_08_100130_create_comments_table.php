31614b9c2376b42fb51126960d2bbc5f404dd950
sayeban300300@gmail.com <sayeban300300@gmail.com>
2021-12-09 08:58:56 +0330

    define foreign key

--- database/migrations/2021_12_08_100130_create_comments_table.php
+++ database/migrations/2021_12_08_100130_create_comments_table.php
@@ -13,10 +13,16 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
+        Schema::enableForeignKeyConstraints();
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+
             $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->Constraints('users')->onDelete('cascade')->onUpdate('cascade');
+
             $table->unsignedBigInteger('ads_id');
+            $table->foreign('ads_id')->Constraints('advertisements')->onDelete('cascade')->onUpdate('cascade');
+
             $table->string('body',1000);
             $table->boolean('is_status');
             $table->timestamps();
