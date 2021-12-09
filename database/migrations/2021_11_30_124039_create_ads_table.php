5acf1561722b1e171b60de385458331bfaa2ecc1
sayeban300300@gmail.com <sayeban300300@gmail.com>
2021-12-09 08:58:11 +0330

    add schema for foeign ker and change keies

--- database/migrations/2021_11_30_124039_create_ads_table.php
+++ database/migrations/2021_11_30_124039_create_ads_table.php
@@ -12,16 +12,16 @@ class CreateAdsTable extends Migration
      * @return void
      */
     public function up()
-    {
+    {   Schema::enableForeignKeyConstraints();
         Schema::create('ads', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
 
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('user_id')->Constraints('users')->onDelete('cascade')->onUpdate('cascade');
 
             $table->unsignedBigInteger('category_id');
-            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('category_id')->Constraints('categories')->onDelete('cascade')->onUpdate('cascade');
 
             $table->char('title', 27);
             $table->string('desc', 303);
