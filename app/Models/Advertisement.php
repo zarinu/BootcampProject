ae304849a8366f5e2d14f6f054ac27e957362c89
zahra heydari <zheydari5701@gmail.com>
2021-12-10 15:15:55 +0330

    check for relations in advertisement Controller

--- app/Models/Advertisement.php
+++ app/Models/Advertisement.php
@@ -7,19 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Advertisement extends Model
 {
     use HasFactory;
-    // public function user()
-    // {
-    //     return $this->belongsTo(User::class, 'user_id');
-    // }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
 
     public function Category()
     {
         return $this->belongsTo(Category::class);
     }
-    public function Comments()
+    public function Comment()
     {
-        return $this->hasMany(Comments::class);
+        return $this->hasMany(Comment::class, 'ads_id');
     }
 }
 
