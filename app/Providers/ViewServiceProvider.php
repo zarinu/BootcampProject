277bfd482979e868e3fdf46a37f56df0321c5dac
sayeban300300@gmail.com <sayeban300300@gmail.com>
2021-12-12 15:57:40 +0330

    create  this file for define global view

---
+++ app/Providers/ViewServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+
+use App\Models\Category;
+use Illuminate\Support\Facades\View;
+use App\View\Composers\ProfileComposer;
+use Illuminate\Support\ServiceProvider;
+
+class ViewServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // define global by cluser
+        View::composer('partials.sideBar', function ($view) {
+            $view->with('categories', Category::all());
+
+        });
+    }
+}
