c576cb338b414429d63ad3c2ff9da060822ab115
zahra heydari <zheydari5701@gmail.com>
2021-12-13 22:01:59 +0330

    test for cache get and put and work on helper method with array in php

--- app/Http/Controllers/Front/AdvertisementController.php
+++ app/Http/Controllers/Front/AdvertisementController.php
@@ -6,14 +6,19 @@ use App\Http\Controllers\Controller;
 use App\Models\{Advertisement, Category, User, Comment};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Cache;
 
 class AdvertisementController extends Controller
 {
     // for all user can see it complete tomoroow
     public function index()
     {
-
-        // dd(User::pluck('name', 'id')->toSql();;rawSql);
+        // $value = Category::all();
+        // $array = ['haie' => 'sdkjf', 'skdfj' => 'skds', ['skjg' => 'sldkfj', 'skdfj' => 'skfdf']];
+        // Cache::put('zahra', $value, $seconds = 60);
+        // $value = Cache::get('zahra');
+        // dd($value);
         $ads = Advertisement::paginate(6);
         return view('allAds.index', compact('ads'));
     }
