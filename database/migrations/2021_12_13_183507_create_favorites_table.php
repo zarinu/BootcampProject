e7d6fe46aa6ac82c5b157ff7a9cac02ceacf2bb1
zahra heydari <zheydari5701@gmail.com>
2021-12-14 18:19:09 +0330

    generate migration for favorites table

--- database/migrations/2021_12_13_183507_create_favorites_table.php
+++ database/migrations/2021_12_13_183507_create_favorites_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\{Advertisement, User};
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,8 +16,13 @@ class CreateFavoritesTable extends Migration
     {
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
+            
+            $table->foreignId('user_id')->constrained('users')->onUpdate('cascade');
+            $table->foreignId('ads_id')->constrained('advertisements')->onUpdate('cascade');
+
             $table->timestamps();
         });
+        Schema::enableForeignKeyConstraints();
     }
 
     /**
