2a38abf9b0ce57fbbcf00bef1a8acaa64b0e1bf8
sayeban300300@gmail.com <sayeban300300@gmail.com>
2021-12-15 04:10:07 +0330

    define spicial header for userpanel

---
+++ resources/views/partials/headerUserPanel.blade.php
@@ -0,0 +1,39 @@
+<nav class="navbar navbar-expand-sm bg-secondary navbar-dark mt-3">
+    {{-- for user panel setting --}}
+    <div >
+      <a class="navbar-brand" href="#">
+        <img src="/images/userImage.png" alt="Logo" style="width:40px;" class="rounded-pill">
+      </a>
+    </div>
+       {{-- define links for pro page --}}
+       <div class="collapse navbar-collapse" id="mynavbar">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="{{route('ads.create')}}">CreateNewAds</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">MyAds</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Favorite</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('ads.category')}}">Category</a>
+            </li>
+       </div>
+        {{-- search bar nav --}}
+
+        <form class="d-flex justify-content-center mr-5">
+          <input class="form-control me-2" type="text" placeholder="Search">
+          <button class="btn ml-2" type="button" style="background-color:#EF7C8E">Search</button>
+        </form>
+
+    {{-- dropdown button show --}}
+    <div style="float: right">
+        <a href="#"><button class="btn" style="background-color:#EF7C8E" type="button">LogOut
+          </button></a>
+    </div>
+
+  </nav>
+
+
