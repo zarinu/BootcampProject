4b464a96120d8ba33b2673751f7232cdad0c27c2
zahra heydari <zheydari5701@gmail.com>
2021-12-21 23:50:00 +0330

    fix view show.blade in common folder

--- resources/views/common/show.blade.php
+++ resources/views/common/show.blade.php
@@ -1,36 +1,33 @@
 @extends('layouts.master')
 @section('content')
-
 <div class="card">
     <div class="card-header">
         <div class="d-inline-flex p-3">
-            <h5>Title:</h5>
-            <div class="p-2">{{$ade->title}}</div>
+            <h3>{{$ade->title}}</h3>
         </div>
     </div>
     <div class="card-body">
         <div class="d-flex flex-column ">
-            <h5>Mobile Number:</h5>
-            <div class="p-2">{{$ade->mobileNo}}</div>
-            <h5>Price:</h5>
-            <div class="p-2">{{$ade->price}}</div>
-            <h5>Description:</h5>
-            <div class="p-2">{{$ade->desc}}</div>
-            <h5>Addres:</h5>
-            <div class="p-2">{{$ade->adress}}</div>
+            <h5>{{$ade->getCreated_at()}} | {{$ade->adress}} | {{$ade->category->name}}</h5><br>
+            <h6>moblie number : {{$ade->mobileNo}} | price : {{$ade->price}} |
+                <a href="{{route('favorite.store')}}">Favorite This | </a>
+                <a href="#comments">Commnets</a>
+            </h6><br>
+            <p><strong>description : </strong>{{$ade->desc}}</p>
         </div>
     </div>
-    <div class="card">
-        <h2>All Comments :</h2>
-        @foreach ($comments as $comment)
-        <div class="card-body">
-            <p>{{$comment->body}}</p>
-            <p>added by : {{$comment->user->name}}</p>
-            <br>
+</div><br>
+<div class="card">
+    <div class="card-header">
+        <div class="d-inline-flex p-3">
+            <h3 id="comments">All Comments </h3>
         </div>
+    </div>
+    <div class="card-body">
+        @foreach ($comments as $comment)
+        <p>{{$comment->body}} <strong>Added by : {{$comment->user->name}}</strong></p>
         @endforeach
         <a href="{{route('comment.create', ['id' => $ade->id])}}">Add a comment</a>
     </div>
 </div>
-{{-- THIS PLACE FOR SHOW ALL COMMENTS --}}
 @endsection
