4b003df98204bdba92f0663f8ad3f6293a2010ac
zahra heydari <zheydari5701@gmail.com>
2021-12-21 22:11:38 +0330

    add 'mahboob tarin ha' section in index.blade common

--- resources/views/layouts/partials/sideBar.blade.php
+++ resources/views/layouts/partials/sideBar.blade.php
@@ -1,6 +1,11 @@
-<div>
-    <p>Category</p>
-</div>
+<h4>Filters</h4><hr>
+<h6>Important</h6>
+<ul class="list-group">
+    <li class="list-group-item">
+        <a href="#" class="categories">محبوب ترین ها</a>
+    </li>
+</ul><br>
+<h6>Category</h6>
 <ul class="list-group">
     @foreach ($categories as $category )
     <li class="list-group-item">
