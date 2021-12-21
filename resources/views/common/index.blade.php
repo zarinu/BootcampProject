5b0a96ef57de68d8eee5b2363087bd7b7ccd4788
zahra heydari <zheydari5701@gmail.com>
2021-12-21 16:54:36 +0330

    edit index.blade.php at common folder for nice views

--- resources/views/common/index.blade.php
+++ resources/views/common/index.blade.php
@@ -1,30 +1,22 @@
 @extends('layouts.master')
 @section('content')
-     @foreach ($ads as $ade)
-    <div class="d-inline-flex p-2">
-        <div class="card " style="width:250px;">
-            <div class="card-body">
-                <h3 class="card-title">{{$ade->title}}</h3>
+@foreach ($ads as $ade)
+<div class="d-inline-flex p-2">
+    <div class="card" style="width:250px;">
+        <form action="{{route('show', ['id' => $ade->id])}}" id="{{$ade->id}}" method="GET">
+            <div class="card-body"  style="cursor: pointer;" onclick="(function(){
+                document.getElementById('{{$ade->id}}').submit();})();">
+                <h3 class="card-title" style="height: 60px;">{{$ade->title}}</h3>
                 <hr>
-                <p class="card-text">{{$ade->desc}}
-                   <hr>
-                    {{$ade->price}}
-                </p>
-                <div class="d-flex flex-row ">
-                    <form action="{{route('show', ['id' => $ade->id])}}" method="GET">
-                        @csrf
-                        <input type="hidden"  name="id" value="{{$ade->id}}">
-                        <button type="submit" class="btn btn-danger">Show Ads</button>
-                    </form>
-                    <a style="color:white" href="{{route('home')}}">
-                        <button type="submit"  style="margin-left:75px" class="btn btn-danger"><i class="fa fa-heart-o" aria-hidden="true"></i>
-                    </a>
-                </div>
+                <h5>{{$ade->price}}</h5>
+                <hr>
+                <h6 style="height: 40px;">{{$ade->getCreated_at()}} <strong>in</strong> {{$ade->adress}}</h6>
             </div>
-        </div>
+        </form>
     </div>
-    @endforeach
-    <ul class="pagination justify-content-center" style="margin:20px 0">
-        <li class="page-item">{{$ads->links()}}</li>
-    </ul>
+</div>
+@endforeach
+<ul class="pagination justify-content-center" style="margin:20px 0">
+    <li class="page-item">{{$ads->links()}}</li>
+</ul>
 @endsection
