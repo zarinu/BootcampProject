bfd4aa27f7572bdb15f3840c44bc0eebc49160df
zahra heydari <zheydari5701@gmail.com>
2021-12-22 11:07:41 +0330

    mage chand ta kolan ye user agahi misaze ke pagination and link bezarim

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,39 +1,38 @@
 @extends('user.layouts.master')
 @section('content')
-     @foreach ($ads as $ade)
-    <div class="pt-2">
-        <div class="card ">
+@foreach ($ads as $ade)
+<div class="pt-2">
+    <div class="card ">
 
-            <div class="card-header">
-                <h3 class="card-title"> <p>title: {{$ade->title}} </p></h3>
-                {{-- <p class="card-text">{{$ade->desc}} --}}
-            </div>
-                <div class="card-body ">
-                    <p>address: {{$ade->adress}}</p>
-                   <hr>
-                   <p>price: {{$ade->price}}</p>
-                    <div class="d-inline-flex btn-group">
-                            <form action="{{route('user.show', ['id' => $ade])}}" method="GET">
-                                @csrf
-                                <input type="hidden"  name="id" value="{{$ade->id}}">
-                                <button type="submit" class="btn btn-secondary " style="margin-right: 50px">Show Ads</button>
-                            </form>
-                        <a href="#">
-                            <button type="button" class="btn btn-secondary">Managment Ads</button>
-                        </a>
-                    </div>
-                    {{-- <form action="{{route('favorite.store', ['id' => $ade])}}" method="post">
-                        @csrf
-                        <input type="hidden"  name="ade-id" value="{{$ade->id}}">
-                        <button type="submit"  style="margin-left:75px" class="btn btn-danger"><i class="fa fa-heart-o" aria-hidden="true"></i>
-                        </button>
-                    </form> --}}
-                </div>
-
+        <div class="card-header">
+            <h3 class="card-title">
+                <p>title: {{$ade->title}} </p>
+            </h3>
+            {{-- <p class="card-text">{{$ade->desc}} --}}
         </div>
+        <div class="card-body ">
+            <p>address: {{$ade->adress}}</p>
+            <hr>
+            <p>price: {{$ade->price}}</p>
+            <div class="d-inline-flex btn-group">
+                <form action="{{route('user.show', ['id' => $ade])}}" method="GET">
+                    @csrf
+                    <input type="hidden" name="id" value="{{$ade->id}}">
+                    <button type="submit" class="btn btn-secondary " style="margin-right: 50px">Show Ads</button>
+                </form>
+                <a href="#">
+                    <button type="button" class="btn btn-secondary">Managment Ads</button>
+                </a>
+            </div>
+            {{-- <form action="{{route('favorite.store', ['id' => $ade])}}" method="post">
+            @csrf
+            <input type="hidden" name="ade-id" value="{{$ade->id}}">
+            <button type="submit" style="margin-left:75px" class="btn btn-danger"><i class="fa fa-heart-o" aria-hidden="true"></i>
+            </button>
+            </form> --}}
+        </div>
+
     </div>
-    @endforeach
-    <ul class="pagination justify-content-center" style="margin:20px 0">
-        <li class="page-item">{{$ads->links()}}</li>
-    </ul>
+</div>
+@endforeach
 @endsection
