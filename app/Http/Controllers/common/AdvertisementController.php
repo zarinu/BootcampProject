e9bd22ad07d31565a620f8a3521853846889ef9e
zahra heydari <zheydari5701@gmail.com>
2021-12-24 15:59:27 +0330

    register event and listener for register new user

--- app/Http/Controllers/common/AdvertisementController.php
+++ app/Http/Controllers/common/AdvertisementController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\common;
 
+use App\Events\NewUserRegisteredEvent;
 use App\Http\Controllers\Controller;
 use App\Models\{Advertisement, Category, User, Comment};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
 
 class AdvertisementController extends Controller
@@ -14,6 +16,7 @@ class AdvertisementController extends Controller
     // for all user can see it complete tomoroow
     public function index()
     {
+        event(new NewUserRegisteredEvent(Auth::user()));
         $currentPage = request()->get('page', 1);
 
         $ads = Cache::remember('common-' . $currentPage, now()->addMinutes(10), function () {
