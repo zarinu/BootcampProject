68136d4bbdf4978af6304a03b35e347462567aee
zahra heydari <zheydari5701@gmail.com>
2021-12-31 11:41:29 +0330

    completation of admin index => show all Ad in /admin/index in admin panel

--- app/Http/Controllers/admin/AdminPanelController.php
+++ app/Http/Controllers/admin/AdminPanelController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Advertisement;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AdminPanelController extends Controller
 {
     //
     public function index() {
-        return view('admin.index');
+        $ads = DB::table('advertisements')->orderBy('id', 'desc')->paginate(8);
+        return view('admin.index', compact('ads'));
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,8 +1,30 @@
 @extends('admin.layouts.master')
 @section('content')
-<div>
-    <a href="#">managmant all Advertisement with allow express an ad</a><br><hr>
-    <a href="{{route('category.index')}}">managmant all Category</a><br><hr>
-    <a href="{{route('comment.indexix')}}">managmant all Commment with allow express an Comment</a>
+@foreach ($ads as $ade)
+<div class="pt-2">
+    <div class="card ">
+
+        <div class="card-header">
+            <h3 class="card-title">
+                <p>title: {{$ade->title}} </p>
+            </h3>
+            {{-- <p class="card-text">{{$ade->desc}} --}}
+        </div>
+        <div class="card-body ">
+            <p>address: {{$ade->adress}}</p>
+            <hr>
+            <p>price: {{$ade->price}}</p>
+            <div class="d-inline-flex btn-group">
+                <a href="{{route('ad.show', ['id' => $ade->id])}}">
+                    <button type="submit" class="btn btn-secondary " style="margin-right: 50px">Show Ads</button>
+                </a>
+            </div>
+        </div>
+
+    </div>
 </div>
+@endforeach
+<ul class="pagination justify-content-center" style="margin:20px 0">
+    <li class="page-item">{{$ads->links()}}</li>
+</ul>
 @endsection
