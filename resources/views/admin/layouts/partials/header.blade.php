e78a57c765d243e476c570465a2c563a9ef6ef52
zahra heydari <zheydari5701@gmail.com>
2021-12-31 11:13:43 +0330

    edit admin header

--- resources/views/admin/layouts/partials/header.blade.php
+++ resources/views/admin/layouts/partials/header.blade.php
@@ -3,10 +3,16 @@
         <p>hi admin ^_^</p>
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="{{route('admin.index')}}">AdminPanel</a>
+                <a class="nav-link" href="{{route('admin.index')}}">AdminPanel:ManageAds</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">AllAds</a>
+                <a class="nav-link" href="{{route('index')}}">AllAds</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('category.index')}}">ManageCategories</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('comment.index')}}">ManageComments</a>
             </li>
         </ul>
     </div>
